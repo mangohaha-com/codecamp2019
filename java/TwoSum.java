b46bd220f16d25eeb2aa5e83a93957427848302d
Da <Yin>
2018-11-08 00:44:51 -0800

    LC1

---
+++ java/TwoSum.java
@@ -0,0 +1,20 @@
+public class TwoSum {
+
+    public static void main(String[] args){
+        twoSum(new int[]{2, 7, 11, 15});
+    }
+
+    // time O(n^2)
+    // space O(1)
+    public static int[] twoSum(int[] nums, int target) {
+        for(int i = 0; i < nums.length; i++){
+            for(int j = 0; j< nums.length; j++){
+                if(i==j) continue;
+                if(nums[i]+nums[j]==target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+}
