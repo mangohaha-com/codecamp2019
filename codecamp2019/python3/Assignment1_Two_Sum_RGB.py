762decf5b3152358ee19d28e77c8a2454eb3684a
BostonRGBBird <fangwang.fang.4@gmail.com>
2018-11-10 07:32:08 -0500

    Assingment1 python3

---
+++ codecamp2019/python3/Assignment1_Two_Sum_RGB.py
@@ -0,0 +1,7 @@
+def Assignment1_Two_Sum_RGBBird(nums, target):
+    for i in range(len(nums)):
+        for j in range(i+1, len(a)):
+            if (nums[i] + nums[j]) == target:
+                print ("Found eligible pair: ", i, j)
+            else:
+                pass
