1d3215527fabe93e93c565f6ee589712c6f3af1e
stacyxixi <wangxiaoxi82@gmail.com>
2018-11-09 22:40:33 -0600

    LC27

---
+++ python2/LC27_RemoveElement_xixi.py
@@ -0,0 +1,29 @@
+"""
+Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+
+"""
+
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        i = 0
+        j = 0
+
+        while i+j < len(nums):
+
+
+            nums[i] = nums[i+j]
+            if nums[i] == val:
+                j += 1
+
+            else: i +=1
+
+        return i
