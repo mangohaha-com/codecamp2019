d24212f97a6b6ea9f54bdebf6c2e0aef7e8de0e5
Arielmfbx3qw9 <32520677+Arielmfbx3qw9@users.noreply.github.com>
2018-11-11 12:26:34 -0800

    Create removeElementAriel.py

---
+++ removeElementAriel.py
@@ -0,0 +1,19 @@
+def removeElement(nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        if len(nums) == 0:
+            return 0
+        j = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[j] = nums[i]
+                j = j+1
+        return j
+        
+       
+nums = [3,2,2,3]
+val = 3
+print(removeElement(nums,val))
