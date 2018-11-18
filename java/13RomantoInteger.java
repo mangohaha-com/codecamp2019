b1972ba23ba7d0e4b05e197f6e7ad1519af34aab
xuesong pan <1367368457@qq.com>
2018-11-17 19:26:54 -0500

    I like this solution

---
+++ java/13RomantoInteger.java
@@ -0,0 +1,28 @@
+class Solution {
+public int romanToInt(String s) {
+     int sum=0;
+    if(s.indexOf("IV")!=-1){sum-=2;}
+    if(s.indexOf("IX")!=-1){sum-=2;}
+    if(s.indexOf("XL")!=-1){sum-=20;}
+    if(s.indexOf("XC")!=-1){sum-=20;}
+    if(s.indexOf("CD")!=-1){sum-=200;}
+    if(s.indexOf("CM")!=-1){sum-=200;}
+    
+    char c[]=s.toCharArray();
+    int count=0;
+    
+   for(;count<=s.length()-1;count++){
+       if(c[count]=='M') sum+=1000;
+       if(c[count]=='D') sum+=500;
+       if(c[count]=='C') sum+=100;
+       if(c[count]=='L') sum+=50;
+       if(c[count]=='X') sum+=10;
+       if(c[count]=='V') sum+=5;
+       if(c[count]=='I') sum+=1;
+       
+   }
+   
+   return sum;
+    
+}
+}
