72539c05d263f803286256bc5e5ba292b2047a52
xuesong pan <1367368457@qq.com>
2018-11-17 19:05:20 -0500

    easy

---
+++ 14LongestCommonPrefix.java
@@ -0,0 +1,26 @@
+class Solution {
+ public String longestCommonPrefix(String[] strs) {
+        StringBuilder result = new StringBuilder();
+        
+        if (strs!= null && strs.length > 0){
+        
+            Arrays.sort(strs);
+            
+            char [] a = strs[0].toCharArray();
+            char [] b = strs[strs.length-1].toCharArray();
+            
+            for (int i = 0; i < a.length; i ++){
+                if (b.length > i && b[i] == a[i]){
+                    result.append(b[i]);
+                }
+                else {
+                    return result.toString();
+                }
+            }
+        }
+        return result.toString();
+    }
+}
+    //suppose the average length of string is m
+    //there are n strings in strs
+    //the time complexity is O(mlog(n))
