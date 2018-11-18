8c0ba0698f4483ba587a3262b63473d7a5bd463e
xuesong pan <1367368457@qq.com>
2018-11-17 19:00:25 -0500

    easy one

---
+++ 83RemoveDepulicates.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head == null) return head;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        while(head != null && head.next != null) {
+            if(head.val == head.next.val) {
+                head.next = head.next.next;
+            }else {
+                head = head.next;
+            }
+        }
+        return dummy.next;
+    }
+}
