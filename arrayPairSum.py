68726229134ac60322d07c58f4b42189bfa527fe
Arielmfbx3qw9 <32520677+Arielmfbx3qw9@users.noreply.github.com>
2018-11-25 14:13:02 -0800

    Create arrayPairSum.py

---
+++ arrayPairSum.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def arrayPairSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        sum = 0
+        for i in range(0, len(nums),2):
+            sum += nums[i]
+        return sum
+            
