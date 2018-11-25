27676d2376e9d1079b4069ccfb274badb2e1241c
Arielmfbx3qw9 <32520677+Arielmfbx3qw9@users.noreply.github.com>
2018-11-24 23:59:15 -0800

    Create removeElement.py

---
+++ removeElement.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        if len(nums) == 0:
+            return 0
+        j = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[j] = nums[i]
+                j = j+1
+        return j
