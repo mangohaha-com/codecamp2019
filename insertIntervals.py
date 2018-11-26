334e06fe7af573c203a46802e7bafeda4b8a1151
Arielmfbx3qw9 <32520677+Arielmfbx3qw9@users.noreply.github.com>
2018-11-26 00:01:20 -0800

    Create insertIntervals.py

---
+++ insertIntervals.py
@@ -0,0 +1,29 @@
+"""
+Definition of Interval.
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    """
+    @param intervals: Sorted interval list.
+    @param newInterval: new interval.
+    @return: A new interval list.
+    """
+    def insert(self, intervals, newInterval):
+        results = []
+        insertPos = 0
+        for interval in intervals:
+            if interval.end < newInterval.start:
+                results.append(interval)
+                insertPos += 1
+            elif interval.start > newInterval.end:
+                results.append(interval)
+            else:
+                newInterval.start = min(interval.start, newInterval.start)
+                newInterval.end = max(interval.end, newInterval.end)
+        print insertPos, newInterval
+        results.insert(insertPos, newInterval)
+        return results
