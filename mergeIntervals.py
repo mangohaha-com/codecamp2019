3d920875dd515f95b864d24ac29249a998aaac14
Arielmfbx3qw9 <32520677+Arielmfbx3qw9@users.noreply.github.com>
2018-11-25 23:41:12 -0800

    Create mergeIntervals.py

---
+++ mergeIntervals.py
@@ -0,0 +1,27 @@
+"""
+Definition of Interval.
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    """
+    @param intervals: interval list.
+    @return: A new interval list.
+    """
+    def merge(self, intervals):
+        intervals.sort(key = lambda x:x.start)
+        length=len(intervals)
+        res=[]
+        for i in range(length):
+            if res==[]:
+                res.append(intervals[i])
+            else:
+                size=len(res)
+                if res[size-1].start<=intervals[i].start<=res[size-1].end:
+                    res[size-1].end=max(intervals[i].end, res[size-1].end)
+                else:
+                    res.append(intervals[i])
+        return res
